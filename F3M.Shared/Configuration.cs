3898d515925230b99df8d97baef17600fd55ca51
FlanderDev <55660647+FlanderDev@users.noreply.github.com>
2026-04-13 06:20:20 +0200

    added build config info

--- F3M.Shared/Configuration.cs
+++ F3M.Shared/Configuration.cs
@@ -2,6 +2,12 @@
 
 public static class Configuration
 {
+    public const string HeaderDisclaimer =
+#if DEBUG
+        "DEVELOPMENT";
+#else
+        "Public Alpha";
+#endif
     public static readonly string[] Categories = ["General", "Audio", "Textures", "Gameplay", "Items", "UI", "Maps", "Characters", "Weapons", "Other"];
     public static readonly string[] AllowedFileFormat = [".zip", ".rar", ".7z", ".pak", ".mod"];
     public static readonly string[] AllowedThumbnailFormat = [".jpg", ".jpeg", ".png", ".webp"];
