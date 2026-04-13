dbf49e41cc8ac44688ade64cc628793aa96a7dcd
FelixLeander <55660647+FelixLeander@users.noreply.github.com>
2026-04-13 04:10:59 +0200

    added component IconAnchor. Moved allowed types to shred.

---
+++ F3M.Shared/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace F3M.Shared;
+
+public static class Configuration
+{
+    public static readonly string[] Categories = ["General", "Audio", "Textures", "Gameplay", "Items", "UI", "Maps", "Characters", "Weapons", "Other"];
+    public static readonly string[] AllowedFileFormat = [".zip", ".rar", ".7z", ".pak", ".mod"];
+    public static readonly string[] AllowedThumbnailFormat = [".jpg", ".jpeg", ".png", ".webp"];
+}
