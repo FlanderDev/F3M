1464e120154d1ef68ec98114d2b04e54aae125d6
FlanderDev <55660647+FlanderDev@users.noreply.github.com>
2026-04-21 00:24:27 +0200

    added SaveModFileDialog

--- F3M.Shared/Configuration.cs
+++ F3M.Shared/Configuration.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace F3M.Shared;
 
@@ -18,6 +17,7 @@ public static class Configuration
     //if (DateTime.TryParse(timeString, out var dt))
     //    return $"Build {dt:yyyy-MM-dd HH:mm:ss}";
 
+    public const string AppName = nameof(F3M);
     public static readonly string[] Categories = ["BepInEx-Plugin", "Custom-Missions 1", "Custom-Missions 2", "Cosplay-Loader", "Texture Edits", "Others"];
     public static readonly string[] AllowedFileFormat = [".zip", ".rar", ".7z", ".pak", ".mod"];
     public static readonly string[] AllowedThumbnailFormat = [".jpg", ".jpeg", ".png", ".webp"];
