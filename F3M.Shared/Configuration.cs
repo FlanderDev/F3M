a180fb807e619dbe5c5d055b16614e9c7cff785f
FlanderDev <55660647+FlanderDev@users.noreply.github.com>
2026-04-20 21:32:07 +0200

    added build timeStamp

--- F3M.Shared/Configuration.cs
+++ F3M.Shared/Configuration.cs
@@ -1,13 +1,23 @@
-﻿namespace F3M.Shared;
+﻿using System.Collections;
+using System.Reflection;
+
+namespace F3M.Shared;
 
 public static class Configuration
 {
-    public const string HeaderDisclaimer =
+    public static string HeaderDisclaimer =>
 #if DEBUG
-        "DEVELOPMENT";
+        $"DEVELOPMENT {BuildTimeStamp}";
 #else
-        "Public Alpha";
+        "Public Alpha {BuildTimeStamp}";
 #endif
+    public static readonly string BuildTimeStamp = Assembly.GetExecutingAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>() is { } aiva
+        ? aiva.InformationalVersion
+        : $"No Build info";
+
+    //if (DateTime.TryParse(timeString, out var dt))
+    //    return $"Build {dt:yyyy-MM-dd HH:mm:ss}";
+
     public static readonly string[] Categories = ["BepInEx-Plugin", "Custom-Missions 1", "Custom-Missions 2", "Cosplay-Loader", "Texture Edits", "Others"];
     public static readonly string[] AllowedFileFormat = [".zip", ".rar", ".7z", ".pak", ".mod"];
     public static readonly string[] AllowedThumbnailFormat = [".jpg", ".jpeg", ".png", ".webp"];
