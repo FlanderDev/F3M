d8822a543d26ccaafc4c13cb77081bae0873b116
FelixLeander <55660647+FelixLeander@users.noreply.github.com>
2026-08-10 06:38:07 +0200

    fixed and slight storage refactor

--- src/F3M.Shared/Helpers/Assets.cs
+++ src/F3M.Shared/Helpers/Assets.cs
@@ -5,12 +5,19 @@
 /// </summary>
 public static class Assets
 {
-    public static string ServerStorage => "Storage";
-    public static string PublicDir => Path.Combine(ServerStorage, nameof(PublicDir));
+    #region Server-side filesystem paths
+    public static string StorageRoot { get; set; } = "Storage";
+    public static string PublicContent { get; set; } = Path.Combine(StorageRoot, nameof(PublicContent));
+    public static string Files { get; set; } = Path.Combine(PublicContent, nameof(Files));
+    public static string Images { get; set; } = Path.Combine(PublicContent, nameof(Images));
 
-    public static string FileDir => Path.Combine(PublicDir, nameof(FileDir));
-    public static string ImageDir => Path.Combine(PublicDir, nameof(ImageDir));
+    public static string FilePath(this string fileName) => Path.Combine(Files, fileName);
+    public static string ImagePath(this string fileName) => Path.Combine(Images, fileName);
+    #endregion
 
-    public static string FilePath(this string fileName) => Path.Combine(FileDir, fileName);
-    public static string ImagePath(this string fileName) => Path.Combine(ImageDir, fileName);
+    #region Browser-facing URLs
+    public const string ServedPath = $"/{nameof(Assets)}";
+    public static string FileUrl(this string fileName) => $"{ServedPath}/{nameof(Files)}/{fileName}";
+    public static string ImageUrl(this string fileName) => $"{ServedPath}/{nameof(Images)}/{fileName}";
+    #endregion
 }
